cf8c8ac5910fbb049b45189f6460ae4d2b37ca95
kajussls121 <82109610+kajussls121@users.noreply.github.com>
2024-06-29 19:33:33 +0100

    Update 2-setup-chroot.sh

--- 2-setup-chroot.sh
+++ 2-setup-chroot.sh
@@ -40,22 +40,24 @@ elif grep -E "Integrated Graphics Controller" <<< ${gpu_type}; then
 elif grep -E "Intel Corporation UHD" <<< ${gpu_type}; then
     pacman -S --needed --noconfirm libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa
 fi
-if [ $(whoami) = "root"  ]; then
-    groupadd libvirt
-    useradd -m -G wheel,libvirt -s /bin/bash $USERNAME
-    echo "user (${USERNAME}) created, home directory created, added to wheel and libvirt group, default shell set to /bin/bash"
-    echo "Enter passwd for $USERNAME"
-    passwd $USERNAME
-fi
+
+groupadd libvirt
+useradd -m -G wheel,libvirt -s /bin/bash $USERNAME
+echo "user (${USERNAME}) created, home directory created, added to wheel and libvirt group, default shell set to /bin/bash"
+echo "Enter passwd for $USERNAME"
+passwd $USERNAME
 cd ~
 systemctl enable NetworkManager.service
-echo "  NetworkManager enabled"
+echo "NetworkManager enabled"
 systemctl enable bluetooth
-echo "  Bluetooth enabled"
+echo "Bluetooth enabled"
 systemctl enable avahi-daemon.service
-echo "  Avahi enabled"
+echo "Avahi enabled"
 echo "Installing grub"
 grub-install --efi-directory=/efi $efi
 echo "Installed grub. Making config."
 grub-mkconfig -o /boot/grub/grub.cfg
 echo "Made grub.cfg"
+pacman -Syy --noconfirm --needed
+pacman -S --noconfirm --needed xorg yay networkmanager dhclient base-devel libx11 libxft libxinerama freetype2 kitty thunar
+systemctl enable --now NetworkManager
